ff1c87d275a142fc98b64493645f0285a1c0b4dd
Prarthana321 <prarthanapadmanabhan@gmail.com>
2024-10-16 14:49:24 +0400

    Create Coding is cool.py
    coding is cool
    Prarthana

---
+++ Exercise 01/Coding is cool.py
@@ -0,0 +1,11 @@
+#Assigning variable word1
+word1 = "coding"
+
+#Assigning variable word2
+word2 = "is"
+
+#Assigning variable word3
+word3 = "cool"
+
+c = word1 + " " + word2 + " " + word3
+print(c)
