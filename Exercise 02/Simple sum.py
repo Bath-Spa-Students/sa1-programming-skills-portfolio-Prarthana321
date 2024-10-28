20e44896d5795c79c02f34886dfbff730f86600b
Prarthana321 <prarthanapadmanabhan@gmail.com>
2024-10-28 09:45:15 +0400

    Create Simple sum.py
    Simple sum

---
+++ Exercise 02/Simple sum.py
@@ -0,0 +1,8 @@
+#Declaring variable a to store integer value 8.
+a=8
+#Declaring variable b to store integer value 10.
+b=10
+#Declaring variable c to store the sum of first two numbers.
+c=a+b
+#Print the sum of two integer.
+print(c)
