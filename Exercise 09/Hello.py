2b96d25f2a8526052801abbce8830bdda0931d12
Prarthana padmanabhan <prarthanapadmanabhan@gmail.com>
2024-11-13 23:34:43 +0400

    Hello

--- Exercise 09/Hello.py
+++ Exercise 09/Hello.py
@@ -2,7 +2,7 @@ def hello():
     print("Hello")  # Fill in this blank to print "Hello" to the console
 
 def main():
-    hello()  # Fill in this blank to call the hello() function
+    hello()  # Fill in this blank to call the hello() function 
 
 if __name__ == "__main__":
     main()
