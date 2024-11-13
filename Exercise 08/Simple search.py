160c0d936fe22355a423756960697140d22446a7
Prarthana padmanabhan <prarthanapadmanabhan@gmail.com>
2024-11-13 23:34:15 +0400

    Simple search

--- Exercise 08/Simple search.py
+++ Exercise 08/Simple search.py
@@ -4,7 +4,7 @@ names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 #Code to input a search term.
 user_search_term = input("Enter a name to search: ")
 
-#Code to Check if the user-provided name is in the list and print the result
+#Code to Check if the user-provided name is in the list and print the result 
 if user_search_term in names:
     print(f"'{user_search_term}' was found in the list.")
 else:
