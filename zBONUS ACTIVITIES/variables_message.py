4556379512b1f10c435dae76cb831bd6369f2b62
Prarthana321 <prarthanapadmanabhan@gmail.com>
2024-11-20 21:52:56 +0400

    Variable Messages

---
+++ zBONUS ACTIVITIES/variables_message.py
@@ -0,0 +1,8 @@
+
+# Assign a message to a variable
+message = "Welcome to the Python world."
+print(message)
+
+# Change the variable's value
+message = "You've just updated the message!"
+print(message)
