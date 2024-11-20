950f21ce433cef87e3d1cf3b302691f421f8312a
Prarthana321 <prarthanapadmanabhan@gmail.com>
2024-11-20 21:49:16 +0400

    Control structure alien

---
+++ zBONUS ACTIVITIES/control_structures_alien.py
@@ -0,0 +1,11 @@
+
+# Define the alien color
+alien_color = "yellow"
+
+# Version that passes
+if alien_color == "yellow":
+    print("You earned 10 points!")
+
+# Version that fails (no output)
+if alien_color == "purple":
+    print("You earned 10 points!")
