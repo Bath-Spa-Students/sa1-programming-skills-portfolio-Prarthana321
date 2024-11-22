382fdbb889b3db9b8efb92c6d0193825d638ec24
Prarthana padmanabhan <prarthanapadmanabhan@gmail.com>
2024-11-22 10:30:32 +0400

    Hello

--- Exercise 09/Hello.py
+++ Exercise 09/Hello.py
@@ -1,8 +1,11 @@
+#This function defines the function name 'hello'.
 def hello():
     print("Hello")  # Fill in this blank to print "Hello" to the console
 
+#This function defines the main function's execution flow.
 def main():
     hello()  # Fill in this blank to call the hello() function 
 
+#This function ensures to run only when the script executes directly.
 if __name__ == "__main__":
     main()
