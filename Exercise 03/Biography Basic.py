69716bebcf2b60d52170ab0a2ad73b9b45c8f537
Prarthana padmanabhan <prarthanapadmanabhan@gmail.com>
2024-11-22 10:10:15 +0400

    Biography Basic

--- Exercise 03/Biography Basic.py
+++ Exercise 03/Biography Basic.py
@@ -1,5 +1,5 @@
 #This is the code for inputing information in a dictionary
-my_information= {"name":"Zeeshan","hometown":"Jamshedpur","age": 22}
+my_information= {"name":"Prarthana","hometown":"Kerala","age": 18}
 
 #This is a code for priningt the dictionary.
 print(f"NAME: {my_information['name']}\nHOMETOWN: {my_information['hometown']}\nAGE: {my_information['age']}")
